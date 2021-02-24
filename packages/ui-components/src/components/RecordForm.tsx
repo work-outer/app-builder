47f417bb140011e421b1c3a8152e4c37704f5cc9
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-02-24 18:54:48 +0800

    通过接口获取object的字段列表

--- packages/ui-components/src/components/RecordForm.tsx
+++ packages/ui-components/src/components/RecordForm.tsx
@@ -17,7 +17,8 @@ export class RecordForm extends React.Component<any> {
     data: []
   };
   componentDidMount = async () => {
-    const url = `http://localhost:8080/api/bootstrap/wspdRw3z3gqkWBWWF/accounts`;
+    const {objectApiName, recordId } = this.props
+    const url = `http://localhost:8080/api/bootstrap/${recordId}/${objectApiName}`;
     const token = `Bearer C6tdnBaPhEFomAWE7,1d48e7c566307d9a80156811c3cf91cead17e0ef04eb4eba8d752e660bbdd580ce00bd0dbd643d2c25f877`
     const res = await steedosClient.doFetch(url,{
       method: 'GET',
@@ -26,15 +27,8 @@ export class RecordForm extends React.Component<any> {
           'Content-Type': 'application/json',
       }
     })
-    //console.log('res--------', res);
-  }
-
-  render() {
-    const {objectApiName, recordId, children, ...rest} = this.props
-   
-    
-    
-    const fields =  data.fields;
+    console.log('res--------', res.fields);
+    const fields =  res.fields;
     const keys =  _.keys(fields);
     const datas = [];
     for(const k in keys ){
@@ -46,9 +40,27 @@ export class RecordForm extends React.Component<any> {
         label: field.label
       });
     }
+
+    this.setState({data:datas})
+  }
+
+  render() {
+    const {objectApiName, recordId, children, ...rest} = this.props
+    const fields =  this.state.data;
+    // const keys =  _.keys(fields);
+    // const datas = [];
+    // for(const k in keys ){
+    //   const field = fields[keys[k]];
+    //   datas.push({
+    //     id: field._id,
+    //     name: field.name,
+    //     type: field.type,
+    //     label: field.label
+    //   });
+    // }
     return (
       <ProForm>
-          {datas.map((d:any) => (
+          {fields.map((d:any) => (
             <InputField 
                  key={d.id}
                  type={d.type}
