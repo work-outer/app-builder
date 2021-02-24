4b55e5e3af75693fd72e3eae4b82292536c6bd47
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-02-24 18:40:56 +0800

    Index 删除无效代码

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -12,5 +12,4 @@ export * from './components/DataTable';
 export * from './components/SteedosContext';
 
 export * from './components/form/InputLookup';
-export * from './components/form/InputLookupTest';
 export * from './utils/';
