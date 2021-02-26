e54b82b53e1fabf0d118b09d4f28bfd5a48a866d
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-02-26 14:31:52 +0800

    RecordForm：字段为lookup

--- packages/ui-components/src/components/RecordForm.tsx
+++ packages/ui-components/src/components/RecordForm.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { InputField } from "..";
+import { FieldLookup, InputField } from "..";
 import ProForm from '@ant-design/pro-form';
 import { SteedosContext } from '..'
+import { Col, Space} from "antd";
 const _ = require('underscore');
 
 export class RecordForm extends React.Component<any, any> {
   static contextType = SteedosContext
   
   state = {
-    data: []
+    conmmonFields: [],
+    specialFields:[]
   };
   componentDidMount = async () => {
     const {objectApiName, spaceId } = this.props;
@@ -24,39 +26,74 @@ export class RecordForm extends React.Component<any, any> {
     })
    
     const fields =  res.fields;
-    console.log('fields--', fields);
     const keys =  _.keys(fields);
-    const datas = [];
+    const conmmonFields = [];
+    const specialFields = [];
     for(const k in keys ){
       const field = fields[keys[k]];
       if(field.hidden) continue;
-      if(typeof field.sort_no === 'undefined') continue;
-      datas.push({
-        name: field.name,
-        type: field.type,
-        label: field.label,
-        sort: field.sort_no,
-        required: field.required
-      });
+      if(typeof field.sort_no === 'undefined' && field.type !== 'lookup') continue;
+      if(field.type === 'lookup'){
+        specialFields.push({
+          name: field.name,
+          type: field.type,
+          label: field.label,
+          referenceTo: field.reference_to,
+          disabled: field.disabled,
+          readonly: field.readonly,
+          omit: field.omit
+        });
+      }else{
+        conmmonFields.push({
+          name: field.name,
+          type: field.type,
+          label: field.label,
+          sort: field.sort_no,
+          required: field.required
+        });
+      }
     }
-    const sortedData = datas.sort((a, b) => a.sort >= b.sort ? 1 : -1);
-    this.setState({data:sortedData})
+    const sortedConmmonFields = conmmonFields.sort((a, b) => a.sort >= b.sort ? 1 : -1);
+    this.setState({
+      conmmonFields: sortedConmmonFields,
+      specialFields: specialFields
+    })
   }
 
   render() {
     const {objectApiName, recordId, children, ...rest} = this.props
-    const fields =  this.state.data;
+    const colSpan = {span:24, xs:24, sm:24, md:12, lg:12};
+    const conmmonFields =  this.state.conmmonFields;
+    const specialFields =  this.state.specialFields;
     return (
       <ProForm>
-          {fields.map((d:any, index) => (
+          {conmmonFields.map((d:any, index) => (
             <InputField
                 key={index}
                 required={d.required} 
                 fieldName={d.name}
                 type={d.type}
                 label={d.label}
+                labelCol
               />
+            
           ))}
+          <Space direction="vertical" size="large" style={{ width: '75%' }}>
+            {specialFields.map((d:any, index) => ( 
+              <Col {...colSpan} >
+                <FieldLookup 
+                  key={index}
+                  name={d.name} 
+                  referenceTo={d.referenceTo}
+                  style={{ width: '100%' }} 
+                  enableAdd={true}
+                  readonly={d.readonly}
+                  disabled={d.disabled}
+                  placeholder={`请搜索${d.name}...`} />
+              </Col>
+            ))}
+          </Space>
+          <br /><br />
       </ProForm>
     )
   }
