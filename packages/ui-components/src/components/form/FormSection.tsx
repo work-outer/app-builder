e010d8ffe31ae4a0dce8f4b1500214169572d453
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-02-27 13:50:51 +0800

    Update FormSection.tsx

--- packages/ui-components/src/components/form/FormSection.tsx
+++ packages/ui-components/src/components/form/FormSection.tsx
@@ -1,7 +1,25 @@
 
 import React from "react";
-import ProCard from '@ant-design/pro-card';
-import { Row } from 'antd';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/accordion";
+import {
+  Box
+} from "@chakra-ui/layout";
+import { FormItemProps } from "./FormItem";
+import { createFormItem } from "./Form";
+import { Grid } from "@chakra-ui/react";
+
+export type FormSectionProps<T = Record<string, any>>  = {
+  fields?: FormItemProps[],
+  columns?:number,
+  formProps?: any,
+  children?: any,
+}
 
 
 export default class FormSection extends React.Component<any> {
@@ -9,14 +27,37 @@ export default class FormSection extends React.Component<any> {
     title: 'Section',
   }
   render() {
-    const {title,  children, ...rest} = this.props
+    const {
+      title,  
+      columns,
+      children, 
+      fields = [], 
+      formProps,
+      ...rest} = this.props
 
     return (
-      <ProCard title={title} ghost collapsible>
-        <Row gutter={[24,0]}>
-          {children}
-        </Row>
-      </ProCard>
+      <Accordion defaultIndex={[0]} allowMultiple colorScheme='gray' pb={2}>
+        <AccordionItem borderBottom={0} borderTop={0}>
+          <Box background='gray.100' borderRadius={2}>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                {title}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </Box>
+          <AccordionPanel py={2} px={0}>
+          <Grid templateColumns={[`repeat(1, 1fr)`, `repeat(${columns}, 1fr)`]} gap={4}>
+            {
+              fields.map((field:FormItemProps) => {
+                return createFormItem(field, formProps)
+              })
+            }
+            {children}
+          </Grid>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     )
   }
 }
