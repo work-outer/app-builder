4e043caddba31152dfc7724ee8d770a11d98b449
zyj805 <zyj805@users.noreply.github.com>
2021-03-04 15:18:03 +0800

    Update FieldSelect.tsx

--- packages/ui-components/src/components/field/FieldSelect.tsx
+++ packages/ui-components/src/components/field/FieldSelect.tsx
@@ -18,8 +18,30 @@ export function FieldSelect(props:FieldSelectProps) {
 export const FieldSelectSettings = {
   name: 'Steedos:FieldSelect',
   inputs: [
-    { name: 'name', type: 'text', defaultValue:'NewFieldSelect'  },
-    { name: 'label', type: 'text' },
+    { name: 'name', type: 'string', defaultValue:'NewField'  },
+    { name: 'required', type: 'boolean' },
+    { name: 'colSpan', type: 'number', defaultValue:'1'},
+    { name: 'help', type: 'string' },
+    { name: 'tooltip', type: 'string' },
+    { name: 'label', type: 'string' },
+    { name: 'placeholder', type: 'string' },
+    // { name: 'layout', type: 'string', enum: ['horizontal', 'vertical']},
+    { name: 'valueEnum',  type: 'list', subFields: [
+      { name: 'label', type: 'string' },
+      { name: 'value', type: 'string' }
+    ]},
+    { name: 'showSearch', type: 'boolean', defaultValue: false },  // 支持搜索
+    // { name: 'allowClear', type: 'boolean', defaultValue: false },  // 支持清除
+    // { name: 'autoFocus', type: 'boolean', defaultValue: false },  // 默认获取焦点
+    { name: 'bordered', type: 'boolean', defaultValue: true },  // 是否有边框
+    // { name: 'defaultActiveFirstOption', type: 'boolean', defaultValue: true },  // 是否默认高亮第一个选项
+    // { name: 'defaultOpen', type: 'boolean' },  // 是否默认展开下拉菜单
+    // { name: 'defaultValue', type: 'list' },  // 指定默认选中的条目
+    // { name: 'disabled', type: 'boolean' },  // 是否禁用
+    // { name: 'dropdownClassName', type: 'string' },  // 下拉菜单的 className 属性
+    // 下拉菜单和选择器同宽。默认将设置 min-width，当值小于选择框宽度时会被忽略。false 时会关闭虚拟滚动
+    // { name: 'dropdownMatchSelectWidth', type: 'string' },  
+    // { name: 'virtual', type: 'boolean', defaultValue: true },  // 设置 false 时关闭虚拟滚动
   ],
   canHaveChildren: false
 }
