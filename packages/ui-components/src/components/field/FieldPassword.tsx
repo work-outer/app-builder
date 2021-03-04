85d12a5a6e5576adc4651323e2843b0856f71c2b
zyj805 <zyj805@users.noreply.github.com>
2021-03-04 16:46:31 +0800

    Update FieldPassword.tsx

--- packages/ui-components/src/components/field/FieldPassword.tsx
+++ packages/ui-components/src/components/field/FieldPassword.tsx
@@ -22,7 +22,14 @@ export function FieldPassword(props:FieldPasswordProps ) {
 export const FieldPasswordSettings = {
     name:'Steedos:FieldPassword',
     inputs:[
-        {name: 'name', type: 'text', defaultValue:'NewFieldPassword'}
+        { name: 'name', type: 'text', defaultValue:'NewFieldPassword'},
+        { name: 'label', type: 'string' },
+        // { name: 'allowClear', type: 'boolean', defaultValue: false },  // 可以点击清除图标删除内容
+        { name: 'bordered', type: 'boolean', defaultValue: true },  // 是否有边框
+        { name: 'disabled', type: 'boolean', defaultValue: false },  // 是否禁用状态，默认为 false
+        { name: 'id', type: 'string' },  // 输入框的 id
+        { name: 'maxLength', type: 'number' },  // 最大长度
+        { name: 'size', type: 'string', enum:['large', 'middle', 'small'] },  // 控件大小
     ],
     canHaveChildren: false
 }
