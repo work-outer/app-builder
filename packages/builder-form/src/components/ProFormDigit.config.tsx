36c0ebd5293ed805888835f77e3467b5144f8ed8
zyj805 <zyj805@users.noreply.github.com>
2021-03-05 17:09:58 +0800

    Update ProFormDigit.config.tsx

--- packages/builder-form/src/components/ProFormDigit.config.tsx
+++ packages/builder-form/src/components/ProFormDigit.config.tsx
@@ -7,6 +7,17 @@ export const configProFormDigit = {
     { name: 'label', type: 'text', defaultValue: 'Digit Field'},
     { name: 'min', type: 'number' },  // 
     { name: 'max', type: 'number' },  // 
+    { name: 'fieldProps', type: 'object', subFields: [
+      { name: 'bordered', type: 'boolean', defaultValue: true },
+      { name: 'decimalSeparator', type: 'string' },
+      { name: 'defaultValue', type: 'number' },
+      { name: 'disabled', type: 'boolean', defaultValue: false },
+      { name: 'keyboard', type: 'boolean', defaultValue: false },
+      { name: 'precision', type: 'number' },
+      { name: 'size', type: 'string', enum: ['large', 'middle', 'small']},
+      { name: 'step', type: 'number', defaultValue: 1 },
+      // { name: 'stringMode', type: 'boolean', defaultValue: false },
+    ]}
   ],
   canHaveChildren: false
 };
