effde7fca8488944797c8de4e0d9242d24e72590
zyj805 <zyj805@users.noreply.github.com>
2021-03-05 11:38:09 +0800

    Update ProFormSwitch.config.tsx

--- packages/builder-form/src/components/ProFormSwitch.config.tsx
+++ packages/builder-form/src/components/ProFormSwitch.config.tsx
@@ -1,7 +1,11 @@
 export const configProFormSwitch = {
   name: 'Steedos:FormSwitch',
   inputs: [
-    { name: 'name', type: 'text', defaultValue: 'switch'},
-    { name: 'label', type: 'text', defaultValue: 'Switch'},
+    { name: 'name', type: 'string', defaultValue: 'switch'},
+    { name: 'label', type: 'string', defaultValue: 'Switch'},
+    { name: 'className', type: 'string'},
+    { name: 'defaultChecked', type: 'boolean'},
+    { name: 'disabled', type: 'boolean'},
+    { name: 'size', type: 'string', enum: ['default', 'small']}
   ],
 };
