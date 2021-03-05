9337528bfaf98a473953de55f33e410be66759f3
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-03-05 19:51:45 +0800

    Update ProForm.config.tsx

--- packages/builder-form/src/components/ProForm.config.tsx
+++ packages/builder-form/src/components/ProForm.config.tsx
@@ -2,7 +2,8 @@ export const configProForm = {
   name: 'Steedos:Form',
   inputs: [
     { name: 'title', type: 'text' },
-    { name: 'initialValues', type: 'code', language: 'javascript', defaultValue: "({})"},
+    { name: 'layout', type: 'string', defaultValue: 'horizontal', enum: ['horizontal', 'vertical', 'inline']},
+    { name: 'initialValues', helperText:'表单默认值，只有初始化以及重置时生效。', type: 'code', language: 'javascript', defaultValue: "({})"},
     { name: 'onValuesChange', type: 'code', language: 'javascript', defaultValue: "(values) => {  }"},
     { name: 'onFinish', type: 'code', language: 'javascript', defaultValue: "(values) => {  }"},
   ],
