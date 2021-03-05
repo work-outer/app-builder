6cdaa10b394ee2a1b48cc9a57cf38e099716d5b4
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-03-05 16:57:41 +0800

    Update ProForm.tsx

--- packages/builder-form/src/components/ProForm.tsx
+++ packages/builder-form/src/components/ProForm.tsx
@@ -2,12 +2,14 @@ import ProForm from '@ant-design/pro-form';
 
 export default function ProFormWrap(props: any) {
   let {initialValues, onFinish, onValuesChange, children, ...rest} = props
-  if (typeof initialValues === 'string')
-    initialValues = eval(initialValues)
-  if (typeof onFinish === 'string')
-    onFinish = eval(onFinish)
-  if (typeof onValuesChange === 'string')
-    onValuesChange = eval(onValuesChange)
+  try {
+    if (typeof initialValues === 'string')
+      initialValues = eval(initialValues)
+    if (typeof onFinish === 'string')
+      onFinish = eval(onFinish)
+    if (typeof onValuesChange === 'string')
+      onValuesChange = eval(onValuesChange)
+  } catch(e){}
 
   return <ProForm initialValues={initialValues} onFinish={onFinish} onValuesChange={onValuesChange} {...rest}>
     {children}
