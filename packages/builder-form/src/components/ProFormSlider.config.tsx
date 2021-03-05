9b448df6d5985943c77f7b421ba4c9b3c6d7696d
zyj805 <zyj805@users.noreply.github.com>
2021-03-05 18:38:34 +0800

    Update ProFormSlider.config.tsx

--- packages/builder-form/src/components/ProFormSlider.config.tsx
+++ packages/builder-form/src/components/ProFormSlider.config.tsx
@@ -5,10 +5,20 @@ export const configProFormSlider = {
     { name: 'readonly', type: 'boolean', defaultValue: false, enum:[true, false]},
     { name: 'width', type: 'string', defaultValue: 'md', enum: ['xs', 'sm', 'md', 'lg', 'xl']},
     { name: 'label', type: 'text', defaultValue: 'Slider'},
+    { name: 'min', type: 'number', defaultValue: 0 },
+    { name: 'max', type: 'number', defaultValue: 100 },
+    { name: 'step', type: 'number', defaultValue: 1 },
     { name: 'marks',  type: 'list', subFields: [
       { name: 'label', type: 'string' },
       { name: 'value', type: 'number' }
     ]},
+    { name: 'fieldProps', type: 'object', subFields: [
+      { name: 'allowClear', type: 'boolean', defaultValue: false },
+      { name: 'disabled', type: 'boolean', defaultValue: false },
+      { name: 'dots', type: 'boolean', defaultValue: false },
+      // { name: 'vertical', type: 'boolean', defaultValue: false }, // todo 未生效
+      { name: 'range', type: 'boolean', defaultValue: false },
+    ]}
   ],
   canHaveChildren: false
 };
