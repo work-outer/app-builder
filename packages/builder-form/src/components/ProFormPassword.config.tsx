75adf58f4d6f64ed945690bb67bf94e57ccbd763
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-03-05 18:04:39 +0800

    增加fieldProps属性

--- packages/builder-form/src/components/ProFormPassword.config.tsx
+++ packages/builder-form/src/components/ProFormPassword.config.tsx
@@ -5,7 +5,14 @@ export const configProFormPassword = {
     { name: 'readonly', type: 'boolean', defaultValue: false, enum:[true, false]},
     { name: 'width', type: 'string', defaultValue: 'md', enum: ['xs', 'sm', 'md', 'lg', 'xl']},
     { name: 'label', type: 'text', defaultValue: 'Password'},
-    { name: 'placeholder', type: 'string'}
+    { name: 'placeholder', type: 'string'},
+    { name: 'fieldProps', type: 'object', subFields: [
+      { name: 'allowClear', type: 'boolean', defaultValue: false},
+      { name: 'size', type: 'string', defaultValue:'middle', enum: ['large', 'middle', 'small']},
+      { name: 'bordered', type: 'boolean', defaultValue: true},
+      { name: 'maxLength', type: 'number'},
+      { name: 'defaultValue', type: 'string'}
+    ]}
   ],
   canHaveChildren: false
 };
--- packages/builder-form/src/components/ProFormText.config.tsx
+++ packages/builder-form/src/components/ProFormText.config.tsx
@@ -5,7 +5,13 @@ export const configProFormText = {
     { name: 'readonly', type: 'boolean', defaultValue: false, enum:[true, false]},
     { name: 'width', type: 'string', defaultValue: 'md', enum: ['xs', 'sm', 'md', 'lg', 'xl']},
     { name: 'label', type: 'text', defaultValue: 'Text Field'},
-    { name: 'placeholder', type: 'string'}
+    { name: 'placeholder', type: 'string'},
+    { name: 'fieldProps', type: 'object', subFields: [
+      { name: 'allowClear', type: 'boolean', defaultValue: false},
+      { name: 'size', type: 'string', defaultValue:'middle', enum: ['large', 'middle', 'small']},
+      { name: 'bordered', type: 'boolean', defaultValue: true},
+      { name: 'defaultValue', type: 'string'}
+    ]}
   ],
   canHaveChildren: false
 };
