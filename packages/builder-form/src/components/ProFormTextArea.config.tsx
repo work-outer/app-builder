0ad9bcfe67de729de310334610dc197864ec58ff
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-03-05 10:57:45 +0800

    ProFormTimePicker，ProFormTextArea

---
+++ packages/builder-form/src/components/ProFormTextArea.config.tsx
@@ -0,0 +1,8 @@
+export const configProFormTextArea = {
+  name: 'Steedos:FormTextArea',
+  inputs: [
+    { name: 'name', type: 'text', defaultValue: 'textarea'},
+    { name: 'label', type: 'text', defaultValue: 'TextArea'},
+    { name: 'placeholder', type: 'string'}
+  ],
+};
--- packages/builder-form/src/index.tsx
+++ packages/builder-form/src/index.tsx
@@ -1,5 +1,5 @@
 import { Builder, withChildren } from '@builder.io/react';
-import ProForm, {ProFormDatePicker, ProFormDateRangePicker, ProFormDateTimePicker, ProFormDateTimeRangePicker, ProFormText} from '@ant-design/pro-form';
+import ProForm, {ProFormDatePicker, ProFormDateRangePicker, ProFormDateTimePicker, ProFormDateTimeRangePicker, ProFormText, ProFormTextArea, ProFormTimePicker} from '@ant-design/pro-form';
 import ProTable from '@ant-design/pro-table';
 import { configProForm } from './components/ProForm.config';
 import { configProFormText } from './components/ProFormText.config';
@@ -9,11 +9,15 @@ import { configProFormDateTimePicker } from './components/ProFormDateTimePicker.
 import { configProFormDateRangePicker } from './components/ProFormDateRangePicker.config';
 import { configProFormDateTimeRangePicker } from './components/ProFormDateTimeRangePicker.config';
 import { configProFormTextPassword } from './components/ProFormText.Password.config';
+import { configProFormTimePicker } from './components/ProFormTimePicker.config';
+import { configProFormTextArea } from './components/ProFormTextArea.config';
 
 Builder.registerComponent(withChildren(ProForm), configProForm);
 Builder.registerComponent(ProFormText, configProFormText);
 Builder.registerComponent(ProFormText.Password, configProFormTextPassword);
+Builder.registerComponent(ProFormTextArea, configProFormTextArea);
 Builder.registerComponent(ProFormDatePicker, configProFormDatePicker);
+Builder.registerComponent(ProFormTimePicker, configProFormTimePicker);
 Builder.registerComponent(ProFormDateTimePicker, configProFormDateTimePicker);
 Builder.registerComponent(ProFormDateRangePicker, configProFormDateRangePicker);
 Builder.registerComponent(ProFormDateTimeRangePicker, configProFormDateTimeRangePicker);
