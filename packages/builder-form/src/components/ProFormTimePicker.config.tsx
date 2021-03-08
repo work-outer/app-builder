85bec415de1b206142d61636240bbcf945960640
zyj805 <zyj805@users.noreply.github.com>
2021-03-08 17:30:03 +0800

    Update ProFormTimePicker.config.tsx

--- packages/builder-form/src/components/ProFormTimePicker.config.tsx
+++ packages/builder-form/src/components/ProFormTimePicker.config.tsx
@@ -1,17 +1,22 @@
 export const configProFormTimePicker = {
   name: '@steedos/builder-form:TimePicker',
   inputs: [
-    { name: 'name', type: 'text', defaultValue: 'time', required: true},
-    { name: 'label', type: 'text', defaultValue: 'Time Picker', required: true},
-    { name: 'readonly', type: 'boolean', defaultValue: false},
-    { name: 'width', type: 'string', defaultValue: 'md', enum: ['xs', 'sm', 'md', 'lg', 'xl']},
-    { name: 'disabled', type: 'boolean', defaultValue:false},
-    { name: 'fieldProps', type: 'object', subFields: [
-      { name: 'allowClear', type: 'boolean', defaultValue: true},
-      { name: 'size', type: 'string', defaultValue:'middle', enum: ['large', 'middle', 'small']},
-      { name: 'autoFocus', type: 'boolean', defaultValue: false},
-      { name: 'inputReadOnly', type: 'boolean', defaultValue: false}
-    ]}
+    { name: 'name', type: 'text', defaultValue: 'time', required: true },
+    { name: 'label', type: 'text', defaultValue: 'Time Picker', required: true },
+    { name: 'readonly', type: 'boolean', defaultValue: false },
+    { name: 'width', type: 'string', defaultValue: 'md', enum: ['xs', 'sm', 'md', 'lg', 'xl'] },
+    { name: 'disabled', type: 'boolean', defaultValue: false, helperText: '禁用' },
+    {
+      name: 'fieldProps', type: 'object', subFields: [
+        { name: 'allowClear', type: 'boolean', defaultValue: true, helperText: '是否显示清除按钮' },
+        {
+          name: 'size', type: 'string', defaultValue: 'middle',
+          enum: ['large', 'middle', 'small'], helperText: '输入框大小，large 高度为 40px，small 为 24px，默认是 32px'
+        },
+        { name: 'autoFocus', type: 'boolean', defaultValue: false, helperText: '自动获取焦点' },
+        { name: 'inputReadOnly', type: 'boolean', defaultValue: false, helperText: '设置输入框为只读（避免在移动设备上打开虚拟键盘）' }
+      ]
+    }
   ],
   defaultStyles: {
     display: 'block',
