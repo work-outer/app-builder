d267beeb0f57d99784bc93dc3772b23c2d38078e
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-03-08 14:31:21 +0800

    Update ProFormPassword.config.tsx

--- packages/builder-form/src/components/ProFormPassword.config.tsx
+++ packages/builder-form/src/components/ProFormPassword.config.tsx
@@ -5,6 +5,7 @@ export const configProFormPassword = {
     { name: 'label', type: 'text', defaultValue: 'Password', required: true},
     { name: 'readonly', type: 'boolean', defaultValue: false},
     { name: 'placeholder', type: 'string'},
+    { name: 'tooltip', type: 'string'},
     { name: 'width', type: 'string', defaultValue: 'md', enum: ['xs', 'sm', 'md', 'lg', 'xl']},
     { name: 'fieldProps', type: 'object', subFields: [
       { name: 'allowClear', type: 'boolean', defaultValue: false},
