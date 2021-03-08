d4e859875722648a45440d0e1281df0f8a08f5ae
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-03-08 11:13:13 +0800

    Update ProForm.Group.cofig.tsx

--- packages/builder-form/src/components/ProForm.Group.cofig.tsx
+++ packages/builder-form/src/components/ProForm.Group.cofig.tsx
@@ -1,5 +1,7 @@
 export const configProFormGroup = {
   name: '@steedos/builder-form:FormSection',
+  image:
+    'https://cdn.builder.io/api/v1/image/assets%2FIsxPKMo2gPRRKeakUztj1D6uqed2%2F682efef23ace49afac61748dd305c70a',
   inputs: [
     { name: 'title', type: 'text', defaultValue: 'Form Section' },
   ],
