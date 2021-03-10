ca75eb6e6b6e0fa5e3e696a87a2573cc720a13a8
Joanna93-JJ <68261251+Joanna93-JJ@users.noreply.github.com>
2021-03-10 11:45:28 +0800

    Field: options

--- packages/builder-form/src/components/Field.config.tsx
+++ packages/builder-form/src/components/Field.config.tsx
@@ -16,6 +16,10 @@ export const configField = {
             'code','switch', 'fromNow','image', 'jsonCode',
           ]
     },
+    { name: 'options',  type: 'list', required: true, subFields: [
+      { name: 'value', type: 'string', defaultValue: 'value' },
+      { name: 'label', type: 'string', defaultValue: 'label' }
+    ], showIf: 'options.get("valueType") === "checkbox" || options.get("valueType") === "radio" || options.get("valueType") === "select" '},
     { name: 'readonly', type: 'boolean', defaultValue: false },
     { name: 'placeholder', type: 'string' },
     { name: 'tooltip', type: 'string' },
