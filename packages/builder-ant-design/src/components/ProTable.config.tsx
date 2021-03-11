7a9bc83d8c4e7807b458f87f79758623d3abd404
殷亮辉 <yinlianghui@hotoa.com>
2021-03-11 17:55:45 +0800

    just format config code

--- packages/builder-ant-design/src/components/ProTable.config.tsx
+++ packages/builder-ant-design/src/components/ProTable.config.tsx
@@ -1,8 +1,8 @@
 export const configProTable = {
   name: '@steedos/builder-form:Table',
   inputs: [
-    { name: 'headerTitle', type: 'string', helperText: '表格名称'},
-    { name: 'size', type: 'string', defaultValue: 'default', enum: ['default', 'middle', 'small']},
+    { name: 'headerTitle', type: 'string', helperText: '表格名称' },
+    { name: 'size', type: 'string', defaultValue: 'default', enum: ['default', 'middle', 'small'] },
     {
       name: 'dateFormatter', type: 'string',
       enum: ['string', 'number', 'false'], helperText: '转化 moment 格式数据为特定类型，false 不做转化'
@@ -27,7 +27,7 @@ export const configProTable = {
     //     { name: 'indentSize', type: 'number', helperText: '展示树形数据时，每层缩进的宽度，以 px 为单位' },
     //   ], helperText: '展开功能的配置'
     // },
-    
+
     {
       name: 'options', type: 'object', subFields: [
         { name: 'fullScreen', type: 'boolean', defaultValue: true },
@@ -52,40 +52,44 @@ export const configProTable = {
       ], helperText: '搜索表单的配置'
     },
 
-    { name: 'bordered', type: 'boolean', defaultValue:true },
-    { name: 'loading', type: 'boolean', defaultValue:false },
+    { name: 'bordered', type: 'boolean', defaultValue: true },
+    { name: 'loading', type: 'boolean', defaultValue: false },
 
-    { name: 'columns', type: 'list',helperText: '列配置', subFields: [
+    {
+      name: 'columns', type: 'list', helperText: '列配置', subFields: [
         { name: 'title', type: 'string', helperText: '标题' },
-        { name: 'dataIndex', type: 'string', helperText: '列数据在数据项中对应的路径'},
-        { name: 'key', type: 'string', helperText: 'React 需要的 key，如果已经设置了唯一的 dataIndex，可以忽略这个属性'},
-        { name: 'width', type: 'number', defaultValue: 60 , helperText: '列宽'},
-        { name: 'align', type: 'string', defaultValue: 'left' , helperText: '对齐方式', enum: ['left', 'right', 'center']},
-        { name: 'valueType', type: 'stirng', 
+        { name: 'dataIndex', type: 'string', helperText: '列数据在数据项中对应的路径' },
+        { name: 'key', type: 'string', helperText: 'React 需要的 key，如果已经设置了唯一的 dataIndex，可以忽略这个属性' },
+        { name: 'width', type: 'number', defaultValue: 60, helperText: '列宽' },
+        { name: 'align', type: 'string', defaultValue: 'left', helperText: '对齐方式', enum: ['left', 'right', 'center'] },
+        {
+          name: 'valueType', type: 'stirng',
           enum: [
-                  'password','money','textarea','option','date',
-                  'dateWeek','dateMonth','dateQuarter','dateYear',
-                  'dateRange','dateTimeRange','dateTime','time',
-                  'timeRange','text','select','checkbox','rate',
-                  'radio','radioButton','index','indexBorder',
-                  'progress','percent','digit','second','avatar',
-                  'code','switch', 'fromNow','image', 'jsonCode',
-                ]
+            'password', 'money', 'textarea', 'option', 'date',
+            'dateWeek', 'dateMonth', 'dateQuarter', 'dateYear',
+            'dateRange', 'dateTimeRange', 'dateTime', 'time',
+            'timeRange', 'text', 'select', 'checkbox', 'rate',
+            'radio', 'radioButton', 'index', 'indexBorder',
+            'progress', 'percent', 'digit', 'second', 'avatar',
+            'code', 'switch', 'fromNow', 'image', 'jsonCode',
+          ]
         },
-        { name: 'sorter', type: 'boolean', defaultValue:false, helperText: '排序' },
-        { name: 'search', type: 'boolean', defaultValue:false, helperText: '搜索' },
-        { name: 'ellipsis', type: 'boolean', defaultValue:false, helperText: '超过宽度将自动省略，暂不支持和排序筛选一起使用' },
-        { name: 'copyable', type: 'boolean', defaultValue:false, helperText: '复制按钮' },
-        { name: 'filters', type: 'list', subFields: [
-            { name: 'text', type: 'string'},
-            { name: 'value', type: 'string'}
+        { name: 'sorter', type: 'boolean', defaultValue: false, helperText: '排序' },
+        { name: 'search', type: 'boolean', defaultValue: false, helperText: '搜索' },
+        { name: 'ellipsis', type: 'boolean', defaultValue: false, helperText: '超过宽度将自动省略，暂不支持和排序筛选一起使用' },
+        { name: 'copyable', type: 'boolean', defaultValue: false, helperText: '复制按钮' },
+        {
+          name: 'filters', type: 'list', subFields: [
+            { name: 'text', type: 'string' },
+            { name: 'value', type: 'string' }
           ], helperText: '表头的筛选菜单项'
         },
-        { name: 'tip', type: 'string', helperText: '提示语'},
-        { name: 'formItemProps', type: 'object', subFields: [
-          { name: 'required', type: 'boolean', defaultValue: false },
+        { name: 'tip', type: 'string', helperText: '提示语' },
+        {
+          name: 'formItemProps', type: 'object', subFields: [
+            { name: 'required', type: 'boolean', defaultValue: false },
           ], helperText: '控制标题对应的输入框'
-        } 
+        }
       ],
     },
   ],
