68043cc8da74703a354429040dd454eced597ee5
pukaijiang <1475194621@qq.com>
2021-03-16 13:37:35 +0800

    jsx

--- packages/builder-form/stories/jsx-lite.stories.tsx
+++ packages/builder-form/stories/jsx-lite.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
-import { componentToCustomElement } from '@jsx-lite/core';
+import { componentToCustomElement, componentToBuilder } from '@jsx-lite/core';
 import { useState, Show } from '@jsx-lite/core';
+import { BuilderComponent } from "@builder.io/react";
 
 export default {
   title: "JSX Lite",
@@ -9,14 +10,55 @@ export default {
 
 export const WebComponentRender = () => {
 
-  const json = require('./test.lite.json');
+  const json = require('./jsx-test.lite.json');
   const template = componentToCustomElement(json);
-  console.log(template)
+  // console.log(template)
 
   if (!window.hasJSX) {
     eval(template)
     window.hasJSX=true
   }
-  return (<my-jsx-component/>)
+  return (<my-component/>)
 }
 
+export const BuilderRender = () => {
+
+  const json = require('./jsx-test.lite.json');
+  const content = componentToBuilder(json);
+  console.log(content)
+
+  const apiKey = 'e9ada5daeb6a4627bc2560d29916c080';
+  const data = {};
+  const buiderProps = {
+    apiKey,
+    content,
+    data,
+    onStateChange: (newData: any) => {
+    }
+  }
+  return (
+    <BuilderComponent {...buiderProps}>
+    </BuilderComponent> 
+  )
+}
+
+
+export const BuilderStatic = () => {
+
+  const content = require('./jsx-test.builder.json');
+  console.log(content)
+
+  const apiKey = 'e9ada5daeb6a4627bc2560d29916c080';
+  const data = {};
+  const buiderProps = {
+    apiKey,
+    content,
+    data,
+    onStateChange: (newData: any) => {
+    }
+  }
+  return (
+    <BuilderComponent {...buiderProps}>
+    </BuilderComponent> 
+  )
+}
